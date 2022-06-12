2548e3e6af0ac530f2059702c49c161299808fb5
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-12 21:18:40 +0600

    Adding dependencies.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import smtplib
+
+my_email = ""
+my_pass = ""
+
+test_email = ""
+with smtplib.SMTP() as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_pass)
+    connection.sendmail(from_addr=my_email, to_addrs=)
