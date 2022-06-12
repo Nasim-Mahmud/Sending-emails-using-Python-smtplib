060fc56b9754982acd71606d0dfa08bd07eded7a
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-12 21:19:47 +0600

    Testing.

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ my_email = ""
 my_pass = ""
 
 test_email = ""
+
 with smtplib.SMTP() as connection:
     connection.starttls()
     connection.login(user=my_email, password=my_pass)
-    connection.sendmail(from_addr=my_email, to_addrs=)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=test_email,
+                        msg="Subject:This is a subject\n\n This is the actual message")
