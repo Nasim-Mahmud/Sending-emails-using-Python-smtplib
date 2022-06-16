857547c4b242da481f5bc164f91b5285f27c042f
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-16 11:41:34 +0600

    Testing.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import smtplib
 
-my_email = "tmailone01@gmail.com"
-my_pass = "b&H24fs}Pf"
+my_email = "tmailtwo02@yahoo.com"
+my_pass = "ingscmtghpaxgmms"
 
-test_email = "tmailtwo02@yahoo.com"
+test_email = "tmailone01@gmail.com"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
+with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
     connection.starttls()
     connection.login(user=my_email, password=my_pass)
     connection.sendmail(from_addr=my_email,
                         to_addrs=test_email,
-                        msg="Subject:This is a subject\n\n This is the actual message")
+                        msg="Subject:This is a subject\n"
+                            "\n This is the actual message")
 
